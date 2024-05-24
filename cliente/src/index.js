36f214b2bb132940124e729d3cac23f276b58649
msaenzre07 <msaenzre07@gmail.com>
2024-05-24 11:12:35 -0600

    Se finaliza formulario de contacto

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -6,11 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Axios from 'axios';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+Axios.defaults.baseURL= 'https://git.heroku.com/friendscarrent.git'
+
 root.render(
   <React.StrictMode>
     <Router>
