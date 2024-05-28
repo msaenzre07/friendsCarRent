f49d317b1b847b56c8168e25b35c8e66cb8be70c
msaenzre07 <msaenzre07@gmail.com>
2024-05-27 20:29:29 -0600

    Vehiculos y Reservas

--- server/src/controllers/usuariosController.js
+++ server/src/controllers/usuariosController.js
@@ -1,15 +1,18 @@
 const User = require('../models/UsuarioModel');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
-  const { nombreCompleto, email, password, archivoLicencia } = req.body; // Agregamos archivoLicencia
+  const { nombreCompleto, email, password, archivoLicencia } = req.body;
 
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ error: 'El correo electrónico ya fue registrado' });
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       nombreCompleto,
       email,
@@ -19,13 +22,37 @@ const register = async (req, res) => {
       tipoIdentificacion: "Cedula",
       cedula: "200000000",
       fechanacimiento: "2000-06-24T00:00:00.000+00:00",
-      archivoLicencia  // Guardamos el nombre del archivo de licencia
+      archivoLicencia
     });
+
     const savedUser = await newUser.save();
     res.status(201).json({ message: 'Usuario registrado correctamente', userId: savedUser._id });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error al registrar el usuario' });
+  }
+}
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'Correo electrónico o contraseña incorrectos' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: 'Correo electrónico o contraseña incorrectos' });
+    }
+
+    const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+
+    res.status(200).json({ message: 'Inicio de sesión exitoso', token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al iniciar sesión' });
   }
 }
 
@@ -38,8 +65,6 @@ const updateUser = async (req, res) => {
       return res.status(404).json({ error: 'Usuario no encontrado' });
     }
 
-    // Actualizar solo los campos proporcionados
-    if (nombreCompleto) user.nombreCompleto = nombreCompleto;
     if (email) {
       const existingUser = await User.findOne({ email });
       if (existingUser && existingUser._id.toString() !== id) {
@@ -47,12 +72,14 @@ const updateUser = async (req, res) => {
       }
       user.email = email;
     }
+
+    if (nombreCompleto) user.nombreCompleto = nombreCompleto;
     if (phone) user.phone = phone;
     if (nacionalidad) user.nacionalidad = nacionalidad;
     if (tipoIdentificacion) user.tipoIdentificacion = tipoIdentificacion;
     if (cedula) user.cedula = cedula;
     if (fechanacimiento) user.fechanacimiento = fechanacimiento;
-    if (archivoLicencia) user.archivoLicencia = archivoLicencia; // Actualizamos el campo de la licencia
+    if (archivoLicencia) user.archivoLicencia = archivoLicencia;
 
     await user.save();
     res.status(200).json({ message: 'Usuario actualizado correctamente' });
@@ -77,4 +104,4 @@ const getUserById = async (req, res) => {
   }
 }
 
-module.exports = { register, updateUser, getUserById };
+module.exports = { register, login, updateUser, getUserById };
